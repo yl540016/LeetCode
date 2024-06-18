b60e2cdc8ded64f5d69317280e8fa7a211fc0b00
Yoojin Lee <64246155+yl540016@users.noreply.github.com>
2024-06-18 16:07:39 -0400

    Time: 488 ms (89.70%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,8 +1,63 @@
 # Write your MySQL query statement below
 
+
+# first login date for each player
+
+
+
 SELECT
     player_id,
-    MIN(event_date) as first_login
+    MIN(event_date) AS first_login
 FROM
     Activity
 GROUP BY player_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# report the first login date for each player
+
+
+# Activity - player_id, device_id, event_date, games_played
+    # p, e - pk
+
+
+# SELECT
+#     player_id, 
+#     MIN(event_date) as first_login
+# FROM
+#     Activity
+# GROUP BY
+#     player_id
+
+
+
+
+
+
+# SELECT
+#     player_id,
+#     event_date as first_login
+# FROM(
+#     SELECT 
+#         player_id,
+#         event_date,
+#     RANK() OVER (
+#         PARTITION BY player_id
+#         ORDER BY event_date
+#     ) AS rank_no FROM Activity ) m
+# WHERE 
+#     rank_no =1
+
